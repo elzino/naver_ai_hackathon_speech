91d693c15fb8482a86b9e2b8b55aeda0ba066438
elzino <ljh0728@snu.ac.kr>
2019-10-07 14:14:07 +0900

    changed dataset

--- run_nsml.sh
+++ run_nsml.sh
@@ -4,7 +4,7 @@ BATCH_SIZE=16
 WORKER_SIZE=2
 GPU_SIZE=1
 CPU_SIZE=4
-DATASET="sr-hack-2019-dataset"
+DATASET="sr-hack-2019-50000"
 MAX_EPOCHS=60
 
 nsml run -g $GPU_SIZE -c $CPU_SIZE -d $DATASET -a "--batch_size $BATCH_SIZE --workers $WORKER_SIZE --max_epochs $MAX_EPOCHS"
