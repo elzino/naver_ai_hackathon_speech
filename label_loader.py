23d482366e131783937b537b91a6db4d417e94f1
elzino <ljh0728@snu.ac.kr>
2019-10-08 18:06:56 +0900

    changed data preprocessing

--- label_loader.py
+++ label_loader.py
@@ -24,21 +24,17 @@ def load_label(label_path):
     index2char = dict() # [id] = ch
     '''
     * 데이터 전처리 *
-    
-    20 이하 (132개) 삭제
-    쓴 옷 밑 > 꽝 삭 줍 윙 앗 헛 째 쑥 캡 클 쭈 e 멜 콕 ; 몽 j \ 댈 옥 흘 뽑 랴 끈 엊 겅 압 떠 띄 즉 튜 푼
-    뜰 핀 잖 팁 흡 곤 쨈 렷 떄 탐 눕 농 칼 쇠 잊 칭 섬 쉐 눌 흰 츠 홓 례 늄 횟 탔 뭣 겸 팥 듣 둔 f 헤 벼 령 퀘 뤄 푸
-    칵 짧 및 쥬 징 ㄴ 썬 짠 앤 롬 렁 팡 r 흠 뗗 뷰 섞 빕 힘 짝 렵 꺾 값 쁘 렬 셧 냅 챙 털 랫 횡 쭉 첫 덕 딜 묶 묘 풀
-    듬 탈 눠 걱 몰 옮 d 딸 퀴 씹 쌍 묻 놨 둬 컴 빅 였 얄 큼 란
-
     특수 케이스 삭제
-    65: ), 132: (, 200: ^, 306: ', 488: /, 662: blank
+    65: ), 132: (, 200: ^, 306: ', 488: /, 662: blank, 722: ㄴ, 745: \ 
     
     특수문자 물음표 -> 일반 물음표
+    >(435) -> ?(123)
+    ;(646) -> ?(123)
     ？(712) -> ?(123)
     '''
-    ban_list = [65, 132, 200, 306, 488, 662]
-    SPECIAL_QUESTION_MARK_INDEX = 712
+
+    ban_list = [65, 132, 200, 306, 488, 662, 722, 745]
+    SPECIAL_QUESTION_MARK_LIST = [435, 646, 712]
     QUESTION_MARK_INDEX = 123
     index = 0
 
@@ -52,16 +48,12 @@ def load_label(label_path):
 
             original_index = int(org_index)
 
-            if int(freq) <= 20 and original_index not in [0, 818, 819]:  # 0 : PAD_TOKEN, 818 : SOS_TOKEN, 819: EOS_TOKEN
-                original2index[original_index] = -1
-                continue
-
             if original_index in ban_list:
                 original2index[original_index] = -1
                 continue
 
-            if original_index is SPECIAL_QUESTION_MARK_INDEX:
-                original2index[SPECIAL_QUESTION_MARK_INDEX] = QUESTION_MARK_INDEX
+            if original_index in SPECIAL_QUESTION_MARK_LIST:
+                original2index[original_index] = QUESTION_MARK_INDEX
                 continue
 
             original2index[original_index] = index
